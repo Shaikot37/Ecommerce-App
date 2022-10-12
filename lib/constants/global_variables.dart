70c5572177605f667773989a1d5baf7a83b0bb67
Shaikot37 <81459697+Shaikot37@users.noreply.github.com>
2022-10-12 23:57:25 +0600

    Update global_variables.dart

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 //String uri = 'http://192.168.1.4:3000';
-String uri = 'https://ecommerce-app-amazon.herokuapp.com/';
+String uri = 'https://ecommerce-cloneapp-api.herokuapp.com';
 
 class GlobalVariables {
   static const appBarGradiant = LinearGradient(
